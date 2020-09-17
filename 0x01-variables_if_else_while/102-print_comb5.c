c98660cb77af04c728c1aff3adfd09cc0f49c3b7
Matias Acosta <2277@holbertonschool.com>
2020-09-17 20:44:39 +0000

    C Task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	int d = 0;
+	int j = 0;
+	int k = 0;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (d = 48; d <= 57; d++)
+		{
+			for (j = 48; j <= 57; j++)
+			{
+				for (k = 48; k <= 57; k++)
+				{
+					if(i == d && j == k)
+					{
+						continue;
+					}
+					if(i ==d && j > k)
+					{
+						continue;
+					}
+					if(i>d)
+					{
+						continue;
+					}
+					putchar(i);
+					putchar(d);
+					putchar(32);
+					putchar(j);
+					putchar(k);
+					putchar(44);
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
