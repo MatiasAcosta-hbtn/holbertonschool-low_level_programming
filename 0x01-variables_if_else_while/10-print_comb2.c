6ef758a1de916523f994ccede89eb96faa7d7ad3
Matias Acosta <2277@holbertonschool.com>
2020-09-17 17:44:20 +0000

    C tasks

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -8,17 +8,25 @@
 
 int main(void)
 {
-	int i = 48;
+	char numbers[] = "0123456789";
+	int i = 0;
+	int d = 0;
 
-	while (i < 58)
+	for(i=0; i<10; i++)
 	{
-		putchar(i);
-		if (i != 57)
+		for(d=0;d<10;d++)
 		{
-			putchar(' ');
-			putchar(',');
+			
+			putchar(numbers[i]);
+			putchar(numbers[d]);
+			if(d !=9 || i != 9)
+			{
+				putchar(' ');
+				putchar(',');
+			}
 		}
-		i++;
+		
+			
 	}
 	putchar('\n');
 	return (0);
