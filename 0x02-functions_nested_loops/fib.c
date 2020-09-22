89f287906259638e030c4625421fc8a1807bc7f5
Matias Acosta <2277@holbertonschool.com>
2020-09-22 22:57:02 +0000

    C functions

---
+++ 0x02-functions_nested_loops/fib.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int count = 0;
+unsigned int a = 0, b = 1, c = 0;
+
+printf("%x", 46618332443);
+printf("\n");
+return (0);
+}
