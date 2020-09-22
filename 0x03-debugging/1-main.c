5200f14b3676d589d3fd894843ffcf5940496578
Matias Acosta <2277@holbertonschool.com>
2020-09-22 16:42:01 +0000

    C functions

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,6 +2,7 @@
 
 /**
 * main - causes an infinite loop
+*
 * Return: 0
 */
 
@@ -17,6 +18,7 @@ int main(void)
 	*        putchar(i);
 	* }
 	*/
+
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
