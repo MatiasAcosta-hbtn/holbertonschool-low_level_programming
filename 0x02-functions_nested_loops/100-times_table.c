663b5dc50dd616e2901ac272fe6cd1164a1bc959
Matias Acosta <2277@holbertonschool.com>
2020-09-22 19:20:07 +0000

    C functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * main - Entry point
- *
+ * print_times_table - print the tables
+ * @n: the number received
  * Return: Always 0 (Success)
  */
 void print_times_table(int n)
@@ -10,24 +10,22 @@ void print_times_table(int n)
 int i, j, sig, res;
 
 n += 48;
-if (n > 48 && n <= 15 + 48){
-
+if (n > 48 && n <= 63)
+{
 	for (i = 48; i <= n; i++)
 	{
 		for (j = 48; j <= n; j++)
 		{
 			res = (i - 48) * (j - 48);
-			sig = (i -48) * ((j+1) - 48);
+			sig = (i - 48) * ((j + 1) - 48);
 			if (res > 99)
-			{	
-				_putchar((res/100) + 48);
-				_putchar(((res /10) % 10) + 48);
+			{
+				_putchar((res / 100) + 48);
+				_putchar(((res / 10) % 10) + 48);
 				 _putchar((res % 10) + 48);
-				
 			}
 			else if (res > 9)
 			{
-		
 				_putchar((res / 10) + 48);
 				_putchar((res % 10) + 48);
 			}
@@ -39,7 +37,7 @@ if (n > 48 && n <= 15 + 48){
 			{
 				_putchar(' ');
 				_putchar(' ');
-			} else if ( res <= 99 && sig <= 99)
+			} else if (res <= 99 && sig <= 99)
 				_putchar(' ');
 			_putchar(' ');
 	}
