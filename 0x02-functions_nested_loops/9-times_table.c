87d03f296508eafca0c7c42666da2d5b96140c11
Matias Acosta <2277@holbertonschool.com>
2020-09-22 16:15:32 +0000

    C functions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,13 +6,14 @@
  */
 void times_table(void)
 {
-int i, j, res;
+int i, j, res, sig;
 
 for (i = 48; i <= 57; i++)
 {
 	for (j = 48; j <= 57; j++)
 	{
 		res = (i - 48) * (j - 48);
+		sig = (i - 48) * ((j + 1) - 48);
 		if (res > 9)
 		{
 			_putchar((res / 10) + 48);
@@ -23,15 +24,12 @@ for (i = 48; i <= 57; i++)
 		if (j != 57)
 		{
 			_putchar(',');
-			if (res <= 9)
-			{
+			if (res <= 9 && sig <= 9 && sig + 48 <= 57)
 				_putchar(' ');
-				_putchar(' ');
+			_putchar(' ');
 			}
-			else
-				_putchar(' ');
 		}
-	}
 	_putchar(10);
 }
 }
+
