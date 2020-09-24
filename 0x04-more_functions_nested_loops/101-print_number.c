bf2928af269aeb4ff73f51a1809db918ee51181c
Matias Acosta <2277@holbertonschool.com>
2020-09-24 21:24:31 +0000

    C functions and loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * pot - pow of the num
+ * @num: number of pow
+ * @potencia: pow of num
+ * Return: the pow
+ */
+
+int pot(int num, int potencia)
+{
+	int i, j = 1;
+
+	for (i = 0; i < (potencia - 1); i++)
+		j *= num;
+	return (j);
+}
+
+/**
+ * print_number - print number like printf
+ * @n: number
+ */
+
+void print_number(int n)
+{
+	int cont = 1, i, dig = n, pow;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	while (dig / 10 != 0)
+	{
+		dig /= 10;
+		cont++;
+	}
+	for (i = cont; i > 0; i--)
+	{
+		pow = pot(10, i);
+		_putchar('0' + (n / pow) % 10);
+	}
+
+}
