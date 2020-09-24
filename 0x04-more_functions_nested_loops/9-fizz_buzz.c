2ab4f8ebde0d55dc9e07495c35325e48a6a0e8c3
Matias Acosta <2277@holbertonschool.com>
2020-09-24 15:37:27 +0000

    C functions and loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int i;
-	
+
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
@@ -21,6 +21,5 @@ int main(void)
 			printf("%i ", i);
 	}
 	return (0);
-	
 }
 
