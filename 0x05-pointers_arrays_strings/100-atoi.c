e2e4953aff21509394473f9094ce0601443964a9
Matias Acosta <2277@holbertonschool.com>
2020-09-30 19:39:12 -0300

    Advanced task

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 
-int _atoi(char *s){
+/**
+ * _atoi - print a number of the string
+ * @s: the string received
+ * Return: the number
+ */
 
-int i=0, j, largo, signo = 1, res, k, inicio, fin, pow, a = 0;
+int _atoi(char *s)
+{
+int i = 0, j = 0, largo = 0, signo = 1, res = 0, k = 0;
+int  inicio = 0, fin = 0, pow = 0, a = 0;
 
-while (*(s + i) != 0){
-	if(*(s + i) == '-')
+while (*(s + i) != 0)
+{
+	if (*(s + i) == '-')
 		signo *= -1;
-	if(*(s + i) >= '0' && *(s + i) <= '9')
+	if (*(s + i) >= '0' && *(s + i) <= '9')
 	{
 		inicio = i;
 		fin = i;
@@ -15,23 +23,20 @@ while (*(s + i) != 0){
 	}
 	i++;
 }
-
-for(k = inicio; *(s + k) != '\0'; k++)
+for (k = inicio; *(s + k) != '\0'; k++)
 {
 	if (*(s + k) >= '0' && *(s + k) <= '9')
 		fin++;
 	else
 		break;
 }
-
 largo = fin - inicio;
-
-for (j = largo; j > 0; j--){
+for (j = 0; j < largo; j++)
+{
 	pow = 1;
-    for (a = 0; a < (j - 1); a++)
-            pow *= 10;
-	res += (s[fin - j] - 48) * pow; 
+	for (a = j; a < largo - 1; a++)
+		pow *= 10;
+	res += (s[inicio + j] - 48) * pow;
 }
-
-return(res * signo);
+return (res * signo);
 }
