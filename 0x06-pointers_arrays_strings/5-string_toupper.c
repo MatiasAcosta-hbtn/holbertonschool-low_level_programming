f50933d699393423a30e712217ac2ada76947b94
Matias Acosta <2277@holbertonschool.com>
2020-10-01 16:02:40 +0000

    More pointers

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,13 +1,20 @@
 #include "holberton.h"
+/**
+ * string_toupper - change the lower to upper
+ * @s: the string
+ * Return: the string changed
+ */
+
 
 char *string_toupper(char *s)
 {
 	int i = 0;
+
 	while (*(s + i) != 0)
 	{
 		if (*(s + i) >= 97 && *(s + i) <= 122)
 			*(s + i) -= 32;
 		i++;
 	}
-	return(s);
+	return (s);
 }
