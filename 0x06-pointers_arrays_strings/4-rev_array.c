c30f712295cec254f816cc02808ac9551c286d69
Matias Acosta <2277@holbertonschool.com>
2020-10-01 15:57:44 +0000

    More pointers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,13 +1,27 @@
 #include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reverse_array - reverse an array
+ * @a: the array
+ * @n: the lenght of the array
+ */
 
 void reverse_array(int *a, int n)
 {
 	int i = 0, j = 0, tin, tfin;
+
 	for (i = n - 1; i >= 0; i--)
 	{
-		tfin = a[i];
-		a[i] = tan;
-		a[j] = tin;
+		tfin = *(a + i);
+		tin = *(a + j);
+		if (j >= i)
+			break;
+		if
+		{
+			*(a + i) = tin;
+			*(a + j) = tfin;
+		}
 		j++;
 	}
-} 
+}
