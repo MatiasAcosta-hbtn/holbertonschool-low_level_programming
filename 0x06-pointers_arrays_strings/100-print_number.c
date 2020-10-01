c7f9733b28655e591cde67320f90aedaf403df6b
Matias Acosta <2277@holbertonschool.com>
2020-10-01 17:45:56 +0000

    More pointers

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * print_number - print number like printf
+ * @n: number
+ */
+
+void print_number(int n)
+{
+	unsigned int cont = 1, i, pow, dig, a;
+
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	dig = n;
+	while (dig / 10 != 0)
+	{
+		dig /= 10;
+		cont++;
+	}
+	for (i = cont; i > 0; i--)
+	{
+		pow = 1;
+		for (a = 0; a < (i - 1); a++)
+			pow *= 10;
+		_putchar('0' + (n / pow) % 10);
+	}
+}
