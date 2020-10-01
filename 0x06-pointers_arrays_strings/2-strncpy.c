bd4dce0c7cf80f1fb4309318f5fc44757a305eed
Matias Acosta <2277@holbertonschool.com>
2020-10-01 15:39:47 +0000

    More pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _strcncpy - function that append the contain in src in the buffer of dest
+ * _strncpy - function that append the contain in src in the buffer of dest
  * @dest: the destination to append
  * @src: the text to copy
  * @n: the numbers of bytes to copy
@@ -9,13 +9,13 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int len = 0, i = 0, j = 0, k = 0;
+	int len = 0, j = 0, k = 0;
 
 	while (*(src + len))
 		len++;
 	if (len > n)
 	{
-		for(j = 0; j < n; j++)
+		for (j = 0; j < n; j++)
 			dest[j] = src[j];
 	}
 	else
