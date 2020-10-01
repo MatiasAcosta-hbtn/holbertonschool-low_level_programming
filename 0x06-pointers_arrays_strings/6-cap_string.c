822e34c9a5738d4c7fb02ff3cae9767e4e17fe8c
Matias Acosta <2277@holbertonschool.com>
2020-10-01 16:26:00 +0000

    More pointers

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,17 +1,30 @@
 #include "holberton.h"
 
+/**
+ * cap_string - capitalize an string
+ * @s: the string received
+ * Return:  the string capitalizado
+ *
+ */
+
 char *cap_string(char *s)
 {
 	int i = 0, j = 0;
-	char puntos[] = ",;.!?\"(){}";
+	int puntos[] = {33, 46, 59, 63, 123, 125, 40, 41, 44, 34, 10, 32, 11};
 
 	while (*(s + i) != 0)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j <= 12; j++)
 		{
-			if(*(s + i) == *(s +j))
-				*(s + i + 2) -= 32;
-		}		
+			if (*(s + i) == puntos[j])
+			{
+				if (*(s + i + 1) >= 97 && *(s + i + 1) <= 122)
+					*(s + i + 1) -= 32;
+				else
+					continue;
+			}
+		}
+		i++;
 	}
-
+	return (s);
 }
