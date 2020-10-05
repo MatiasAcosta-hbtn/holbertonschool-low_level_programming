b83006d9562b3f8b85bf01f81284f8ca34d2c008
Matias Acosta <2277@holbertonschool.com>
2020-10-05 19:56:30 +0000

    More pointers

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,7 +15,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		if (haystack[i] == needle[0])
 		{
-			for (j = 0; *(haystack + j) != '\0' && *(haystack + i + j) != '\0'; j++)
+			for (j = 1; *(haystack + j) != '\0' && *(haystack + i + j) != '\0'; j++)
 			{
 				if (*(haystack + i + j) != *(needle + j))
 					break;
