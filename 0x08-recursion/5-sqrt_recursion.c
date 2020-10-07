4f4bc91b359c66110f71df5e7b262aaab4101f06
Matias Acosta <2277@holbertonschool.com>
2020-10-07 18:41:06 +0000

    Recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,27 +1,33 @@
 #include "holberton.h"
 
 /**
- * aux - auxiliar function                                                                                                                                             * @n: the number
- * @b: the other number                                                                                                                                                * Return: the 1 or 0                                                                                                                                                  */
+ * aux - auxiliar function
+ * @n: a number
+ * @b: an other number
+ * Return: t
+ */ 
 int aux(int n, int b)
 {
 	if (b * b == n)
-		return(b);
-	if(b * b >= n)
-		return(-1);
-	return (aux(n,b + 1);
-
+		return (b);
+	if (b * b > n)
+		return (-1);
+	return (aux(n, b + 1));
 }
 
+/**
+ * _sqrt_recursion - the sqrt of a number
+ * @n: the number
+ * Return: the sqrt of a number
+ */
 int _sqrt_recursion(int n)
 {
-
 	int b = 2;
 
 	if (n <= 0)
 		return (-1);
 	if (n == 1)
 		return (n);
-	return (aux(n,b + 1));
+	return (aux(n, b + 1));
 
 }
