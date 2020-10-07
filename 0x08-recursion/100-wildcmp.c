e5f8d4920f4d9948145c34e187fe18074971e3f3
Matias Acosta <2277@holbertonschool.com>
2020-10-07 21:01:32 +0000

    Recursion

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -8,16 +8,14 @@
 
 int aux(char *s1, char *s2)
 {
-	if(*(s2 + 1) == 0)
-		return(1);
+	if (*(s2 + 1) == 0)
+		return (1);
 	s2++;
 	if (*s1 == 0)
 		return (wildcmp(s1, s2));
 	return (aux(s1 + 1, s2));
 	if (*s1 == *s2)
 		return (wildcmp(s1, s2));
-	
-
 }
 
 /**
