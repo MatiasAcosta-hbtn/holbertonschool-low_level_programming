01a99aaa9bbad3d597903bccd3f888e9d0ccd301
Matias Acosta <2277@holbertonschool.com>
2020-10-07 21:00:18 +0000

    Recursion

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * aux - auxiliar function
  * @s1: string uno
@@ -9,11 +8,15 @@
 
 int aux(char *s1, char *s2)
 {
+	if(*(s2 + 1) == 0)
+		return(1);
+	s2++;
 	if (*s1 == 0)
 		return (wildcmp(s1, s2));
 	return (aux(s1 + 1, s2));
 	if (*s1 == *s2)
 		return (wildcmp(s1, s2));
+	
 
 }
 
@@ -28,14 +31,15 @@ int wildcmp(char *s1, char *s2)
 {
 
 	if (*s2 == '*')
-		return (aux(s1, s2 + 1));
+		return (aux(s1, s2));
 	if (*s1 == 0 || *s2 == 0)
 	{
 		if (*s1 == 0 && *s2 == 0)
 			return (1);
+		return (0);
+	}
 	if (*s1 == *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
-	}
 	return (0);
 }
 
