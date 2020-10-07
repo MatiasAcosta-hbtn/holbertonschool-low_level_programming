4823c0742971770fd4cfd628b5e7c9324eafdf92
Matias Acosta <2277@holbertonschool.com>
2020-10-07 21:55:35 +0000

    Recursion

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,28 +1,5 @@
 #include "holberton.h"
 /**
- *wildcmp -  compares two strings
- *@s1: string 1
- *@s2: string 2
- *Return: 1 if equals otherwise 0
- */
-int wildcmp(char *s1, char *s2)
-{
-	if (*s2 == '*')
-		return (aux2(s1, s2));
-	if (*s2 == '\0' && *s1 != '\0')
-		return (0);
-	if (*s1 == '\0' || *s2 == '\0')
-	{
-		if (*s1 == '\0' && *s2 == '\0')
-			return (1);
-		return (0);
-	}
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	return (0);
-}
-
-/**
  *aux2 - auxiliary function
  *@s1: pointer of string 1
  *@s2: pointer of string 2
@@ -47,4 +24,27 @@ int aux2(char *s1, char *s2)
 	if (*s1 == '\0')
 		return (wildcmp(s1, s2));
 	return (aux2(s1 + 1, s2));
+}
+
+/**
+ *wildcmp -  compares two strings
+ *@s1: string 1
+ *@s2: string 2
+ *Return: 1 if equals otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*')
+		return (aux2(s1, s2));
+	if (*s2 == '\0' && *s1 != '\0')
+		return (0);
+	if (*s1 == '\0' || *s2 == '\0')
+	{
+		if (*s1 == '\0' && *s2 == '\0')
+			return (1);
+		return (0);
+	}
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
 }
