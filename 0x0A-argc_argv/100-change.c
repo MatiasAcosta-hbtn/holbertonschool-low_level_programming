6e132b996531e534724f0341554f686a09723c19
Matias Acosta <2277@holbertonschool.com>
2020-10-10 14:26:37 -0300

    Argc and Argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,26 +2,15 @@
 #include <stdlib.h>
 
 /**
- * main - Entry
- * @argc: count of arguments
- * @argv: arguments passed
- * Return: always 0
+ * contar_monedas - count the cantidad of monedas dependig of the total
+ * @total: the total
+ * Return: the count of monedas
  */
-int main(int argc, char *argv[])
-{
-	int total = 0, monedas = 0;
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	total = atoi(argv[1]);
-	if (total == 0 && *argv[1] != 48)
-	{
-		printf("Error\n");
-		return (1);
-	}
+int contar_monedas(int total)
+{
+	int monedas = 0;
+
 	while (total > 25)
 	{
 		total -= 25;
@@ -47,6 +36,34 @@ int main(int argc, char *argv[])
 		total -= 1;
 		monedas++;
 	}
+return (monedas);
+}
+
+/**
+ * main - Entry
+ * @argc: count of arguments
+ * @argv: arguments passed
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0, total = 0, monedas = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 0; argv[1][i] != 0; i++)
+	{
+		if (argv[1][i] < 48 || argv[1][i] > 57)
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	total = atoi(argv[1]);
+	monedas = contar_monedas(total);
 	printf("%d\n", monedas);
 	return (0);
 }
