78044628f3e38664a26ff7669326a417fd04b3ed
Matias Acosta <2277@holbertonschool.com>
2020-10-14 09:07:04 -0300

    Malloc and free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,5 +9,15 @@
 
 char *str_concat(char *s1, char *s2)
 {
+	char *p;
+	int cont_s1, cont_s2, i, j;
 
+	while(*(s1 + cont_s1) != 0)
+		cont_s1++;
+	while(*(s2 + cont_s2) != 0)
+		cont_s2++;
+	p = malloc(char * (cont_s1 + cont_s2));
+	for (i = 0; i < cont_s1; i++)
+		p[i] = 
+		
 }
