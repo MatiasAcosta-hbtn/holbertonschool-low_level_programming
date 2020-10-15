89c337d3d675f1f82432d1713d4bbd1b075bff4b
Matias Acosta <2277@holbertonschool.com>
2020-10-15 16:22:16 +0000

    More malloc and free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,12 +12,16 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *p;
+	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
-	free(p);
+	for (i = 0; i < size * nmemb; i++)
+	{
+		((char)p)[i] = 0;
+	}
 	return (p);
 }
