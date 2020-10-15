9bce47e7adea29071dc02b6975d1634ea4c82c52
Matias Acosta <2277@holbertonschool.com>
2020-10-15 16:17:30 +0000

    More malloc and free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,21 +15,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
 	unsigned int cont_s1 = 0, cont_s2 = 0, i = 0, j = 0;
+
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	cont_s1 = _strlen(s1);
 	cont_s2 = _strlen(s2);
 	if (n > cont_s2)
 	{
 		n = cont_s2;
 	}
-	p = malloc(sizeof(char) * (cont_s1 + n)+1);
+	p = malloc(sizeof(char) * (cont_s1 + n) + 1);
 	if (p == NULL)
 	{
 		return (NULL);
@@ -45,6 +42,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	p[i + j] = '\0';
 	return (p);
 }
+
+/**
+ * _strlen - count the leng of s
+ * @s: pointer received
+ * Return: the lenght of the character received
+ */
+
 int _strlen(char *s)
 {
 
