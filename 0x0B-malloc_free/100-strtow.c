d76591783f780e0cf7675e6c1c0182abeab4a82f
Matias Acosta <2277@holbertonschool.com>
2020-10-14 23:33:28 -0300

    Malloc and free

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,18 +1,22 @@
 #include "holberton.h"
-
+#include <stdlib.h>
 /**
  * strtow -
  * @str:
  * Return:
- /
+	 */
 
 char **strtow(char *str){
 
 char **p;
 int cont = 0;
 
+if(str == NULL || str[0] == 0)
+	return NULL;
 while(str[cont])
 	cont++;
 p = (char **)malloc(sizeof(char) * cont);
-
+if (p == NULL)
+	return(NULL);
+return(p);
 }
