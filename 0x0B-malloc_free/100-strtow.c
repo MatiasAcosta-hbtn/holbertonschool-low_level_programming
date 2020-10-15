668074de8b5f32f7f34c7d07744c4c4a0735be50
Matias Acosta <2277@holbertonschool.com>
2020-10-14 23:36:19 -0300

    Malloc and free

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,22 +1,22 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- * strtow -
- * @str:
- * Return:
-	 */
+ * strtow - how great i am
+ * @str: the string
+ * Return: the concatenated string or algo asi
+ */
 
-char **strtow(char *str){
+char **strtow(char *str)
+{
+	char **p;
+	int cont = 0;
 
-char **p;
-int cont = 0;
-
-if(str == NULL || str[0] == 0)
-	return NULL;
-while(str[cont])
-	cont++;
-p = (char **)malloc(sizeof(char) * cont);
-if (p == NULL)
-	return(NULL);
-return(p);
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+	while (str[cont])
+		cont++;
+	p = (char **)malloc(sizeof(char) * cont);
+	if (p == NULL)
+		return (NULL);
+	return (p);
 }
