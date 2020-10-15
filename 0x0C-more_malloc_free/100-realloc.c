96f386217da39f3e701d703415327e978fab0fea
Matias Acosta <2277@holbertonschool.com>
2020-10-15 17:23:07 +0000

    More malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -4,10 +4,37 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *p;
+	char *p;
+	unsigned int i = 0;
 	
 	if (new_size == old_size)
 		return(ptr);
-
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL); 
+		return(p);	
+	}
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	p = malloc(new_size);
+	if(p == NULL)
+		return (NULL);
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			p[i] = ((char *)ptr)[i];
+		free(ptr);
+	}
+	if (old_size > new_size)
+	{
+		for (i = 0; i < new_size; i++)
+			p[i] = ((char *)ptr)[i];
+	}
+	return(p);
 
 }
