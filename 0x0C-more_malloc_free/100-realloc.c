edbccde1d98dd4c65055a546ad05c7b0c16ba05d
Matias Acosta <2277@holbertonschool.com>
2020-10-15 17:31:49 +0000

    More malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,19 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * _realloc - reasing a memory to a pointer
+ * @ptr: pointer
+ * @old_size: old size
+ * @new_size: new size
+ * Return: the new array with new size
+ */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *p;
 	unsigned int i = 0;
-	
+
 	if (new_size == old_size)
-		return(ptr);
+		return (ptr);
 	if (ptr == NULL)
 	{
 		p = malloc(new_size);
 		if (p == NULL)
-			return (NULL); 
-		return(p);	
+			return (NULL);
+		return (p);
 	}
 	if (new_size == 0)
 	{
@@ -22,7 +30,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 	p = malloc(new_size);
-	if(p == NULL)
+	if (p == NULL)
 		return (NULL);
 	if (new_size > old_size)
 	{
@@ -35,6 +43,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		for (i = 0; i < new_size; i++)
 			p[i] = ((char *)ptr)[i];
 	}
-	return(p);
+	return (p);
 
 }
