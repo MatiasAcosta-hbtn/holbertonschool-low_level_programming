5cdc25351f3a00ab0e1ba8b0e79db4c56dbb1285
Matias Acosta <2277@holbertonschool.com>
2020-10-15 16:13:24 +0000

    More malloc and free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,23 +11,29 @@ int _strlen(char *s);
  * @n: count of bytes of s2
  * Return: the concatenated strings
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int i = 0, j = 0, cont_s1 = 0, cont_s2 = 0;
-
+	unsigned int cont_s1 = 0, cont_s2 = 0, i = 0, j = 0;
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 	cont_s1 = _strlen(s1);
 	cont_s2 = _strlen(s2);
-	if (n > cont_s2)
-		n = cont_s2;
-	p = (char *)malloc(sizeof(char) * (cont_s1 + n) + 1);
+	if (n > second)
+	{
+		n = second;
+	}
+	p = malloc(sizeof(char) * (cont_s1 + n)+1);
 	if (p == NULL)
+	{
 		return (NULL);
+	}
 	for (i = 0; i < cont_s1; i++)
 	{
 		p[i] = s1[i];
@@ -36,10 +42,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		p[i + j] = s2[j];
 	}
-	p[i + j + 1] = '\0';
+	p[i + j] = '\0';
 	return (p);
 }
-
 int _strlen(char *s)
 {
 
