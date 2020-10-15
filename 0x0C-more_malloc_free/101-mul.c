c4e77466ac35a9a727a19952edce373a6d636829
Matias Acosta <2277@holbertonschool.com>
2020-10-15 18:32:50 +0000

    More malloc and free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * main - entry point
+ * @argc: count elements
+ * @argv: elements
+ * Return: always 0 
+ */ 
 int main(int argc, char *argv[])
 {
 	long int num1, num2, mul;
