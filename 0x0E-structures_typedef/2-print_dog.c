f10eb79988f51a805146322fd52ae2c4353419b5
Matias Acosta <2277@holbertonschool.com>
2020-10-19 18:43:55 -0300

    Structures in C

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,1 +1,20 @@
-void print_dog(struct dog *d);
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print the dog
+ * @d: the dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\nAge: %f\nOwner: %s\n", d->age, d->owner);
+	if (d->age == 0)
+		printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
+	if (d->owner == NULL)
+		printf("Name: %s\nAge: %f\nOwner: (nil)\n", d->name, d->age);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
