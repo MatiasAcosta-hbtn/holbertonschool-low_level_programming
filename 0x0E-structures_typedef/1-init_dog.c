c91559593b645fbb93b32bf7be97219f1a16b154
Matias Acosta <2277@holbertonschool.com>
2020-10-19 18:05:10 -0300

    Structures in C

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - inicialisa a dog
+ * @d: the structure dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
