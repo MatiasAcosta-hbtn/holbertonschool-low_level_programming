8ac2d844a37e801448d53732ce86a5bda87b2606
Matias Acosta <2277@holbertonschool.com>
2020-10-19 16:22:55 -0300

    Structures in C

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,19 @@
-typedef struct dog{
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - the dog structure
+ * @name: the name of dog
+ * @age: the age of dog
+ * @owner: the owner of the dog
+ *
+ * Description: Longer desc
+ */
+typedef struct dog
+{
 char *name;
 float age;
 char *owner;
 } dog;
+#endif
 
 
