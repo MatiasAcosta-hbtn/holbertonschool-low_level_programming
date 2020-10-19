7d98eec5942aef8a36b660162664b47104efe0ef
Matias Acosta <2277@holbertonschool.com>
2020-10-19 19:32:04 -0300

    Structures in C

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,2 +1,8 @@
-dog_t *new_dog(char *name, float age, char *owner);
+#include "dog.h"
 
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t new_dog = {name, age, owner};
+	dog_t *my_dog = &new_dog;
+	return (my_dog);
+}
