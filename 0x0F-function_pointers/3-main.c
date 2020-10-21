cbe1bb9aceb5a8305169a62d7f026a8e32cc0567
Matias Acosta <2277@holbertonschool.com>
2020-10-21 18:40:01 +0000

    Pointers to functions in C

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,18 +1,27 @@
-#include "calc.h"
+#include "3-calc.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-	int num1 = 0, num2 = 0;
 	char op;
 
-	if(argc != 4)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	op = argv[2];
+	if (argv[2][0] != '+' || argv[2][0] != '*' || argv[2][0] != '-' || argv[2][0] != '/' || argv[2][0] != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", (*get_op_func)(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 
 }
