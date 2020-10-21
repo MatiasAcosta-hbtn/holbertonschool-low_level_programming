f3eab18845742c8c95d8e989c0f663c0e0aa2cb1
Matias Acosta <2277@holbertonschool.com>
2020-10-21 20:34:22 +0000

    Pointers to functions in C

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: amount of parameters
+ * @argv: parameters
+ * Return: return always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	unsigned char *p = (char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1] < 0))
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	while (i < (atoi(argv[1]) - 1))
+	{
+		printf("%02x ", *p);
+		i++;
+		p++;
+	}
+	printf("%02x\n", *p);
+	return (0);
+}
