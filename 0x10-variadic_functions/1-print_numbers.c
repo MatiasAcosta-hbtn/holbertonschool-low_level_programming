65a26a0e42f7caef3449df3001cf3fcb70173d9f
Matias Acosta <2277@holbertonschool.com>
2020-10-24 15:44:40 +0000

    Variadic function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,16 +1,23 @@
 #include "variadic_functions.h"
 
+/**
+ * print_numbers - print numbers
+ * @separator: the separator of the numbers
+ * @n: the count of numbers
+ */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int count = 0;
 	va_list args;
 
-	if (separator == NULL)
-		separator = "";
 	va_start(args, n);
 	while (count < n - 1)
 	{
-		printf("%d%s", va_arg(args, int), separator);
+		if (separator == NULL)
+			printf("%d", va_arg(args, int));
+		else
+			printf("%d%s", va_arg(args, int), separator);
 		count++;
 	}
 	printf("%d\n", va_arg(args, int));
