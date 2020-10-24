6b263a731170459482f74a5e009ab70b5a4a0967
Matias Acosta <2277@holbertonschool.com>
2020-10-24 15:45:41 +0000

    Variadic function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,6 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			printf("%d%s", va_arg(args, int), separator);
 		count++;
 	}
-	printf("%d\n", va_arg(args, int));
+	printf("%d", va_arg(args, int));
+	printf("\n");
 	va_end(args);
 }
