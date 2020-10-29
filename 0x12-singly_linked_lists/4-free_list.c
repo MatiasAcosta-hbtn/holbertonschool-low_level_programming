007b1203fbbdbdfda541f9cc9f53942b62d218ac
Matias Acosta <2277@holbertonschool.com>
2020-10-29 19:10:48 +0000

    new commit

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,10 +10,10 @@ void free_list(list_t *head)
 	list_t *tmp;
 
 	while (head != NULL)
-        {
+	{
 		tmp = head;
 		head = head->next;
-        	free(tmp->str);
+		free(tmp->str);
 		free(tmp);
 	}
 	free(head);
