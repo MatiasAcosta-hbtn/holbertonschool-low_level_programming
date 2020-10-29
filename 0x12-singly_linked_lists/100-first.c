c2d5b412346f850c804ba0c37fd9f27e83ab4b27
Matias Acosta <2277@holbertonschool.com>
2020-10-29 16:00:24 +0000

    fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,14 +10,3 @@ void __attribute__ ((constructor)) before()
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
 }
-
-/**
- * main - entry point
- * Return: always 0
- */
-
-int main(void)
-{
-
-return(0);
-}
