f88b63fcf6d9400e3ad92ca393f73a0c6da0fbaf
Matias Acosta <2277@holbertonschool.com>
2020-10-29 17:03:28 +0000

    new commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -7,13 +7,37 @@
  * Return: the list
  */
 
+/**
+ * _strlen - count the leng of s
+ * @s: pointer received
+ * Return: the lenght of the character received
+ */
+int _strlen(const char *s)
+{
+
+	int i = 0;
+
+	while (s[i] != 0)
+		i++;
+	return (i);
+}
+
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_list;
-
-	new_list->str = strdup(str);
-	new_list->len = strlen(str);
-	new_list->next = *head;
-	head = &new_list;
-	return (new_list);
+	list_t *new;
+	if (!str || !head)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	new->str = strdup(str);
+	if (!new->str)
+	{
+		free(new);
+		return(NULL);
+	}
+	new->len = _strlen(str);
+	new->next = *head;
+	*head = new;
+	return (new);
 }
