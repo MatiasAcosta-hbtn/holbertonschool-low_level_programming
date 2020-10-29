013258ffdc06e19ac6ed77d3225a7696798f3c16
Matias Acosta <2277@holbertonschool.com>
2020-10-29 19:41:36 +0000

    new commit

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,10 @@
-%define sys_write 1
+	define sys_write 1
 %define stdout 1
 %define sys_exit 60
 %define success 0
 %define nl 10
 section .data
-    message db "Hello, world!", nl
+    message db "Hello, Holberton", nl
     message_len equ $-message
 section .text
 global main
