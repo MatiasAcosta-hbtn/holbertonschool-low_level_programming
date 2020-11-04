26d74269e502c38863618efcb8cdb17572863a27
Matias Acosta <2277@holbertonschool.com>
2020-11-04 13:17:34 +0000

    Ex 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,6 +1,20 @@
 #include "lists.h"
 
+/**
+ * pop_listint - deleted the head node of a listint?t
+ * @head: the head of the list
+ * Return: head node data(n)
+ */
+
 int pop_listint(listint_t **head)
 {
+	listint_t *deleted, *aux;
 
+	if (*head == NULL)
+		return (0);
+	deleted = *head;
+	*head = deleted->next;
+	free(deleted);
+	aux = *head;
+	return (aux->n);
 }
