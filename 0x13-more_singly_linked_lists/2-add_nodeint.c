14e95cd7756b8ae2cb8515f656ba6ccdf8880853
Matias Acosta <2277@holbertonschool.com>
2020-11-04 12:39:05 +0000

    Fix betty

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * add_nodeint - add a node in the head
+ * @head: pointer to head
+ * @n: the parameter n
+ * Return: pointer to node added
+ */
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *node;
