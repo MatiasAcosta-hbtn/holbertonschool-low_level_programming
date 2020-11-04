0ae4b779652dba2fba567d2b094138d128c01fbc
Matias Acosta <2277@holbertonschool.com>
2020-11-04 18:16:46 +0000

    Singly linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 	aux = *head;
-	while (aux && pos <= idx)
+	while (aux->next && pos < idx)
 	{
 		aux = aux->next;
 		pos++;
