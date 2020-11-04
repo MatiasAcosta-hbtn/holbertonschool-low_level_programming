a206d1881caf6c4e79ce4a6fe758026f6c342bb0
Matias Acosta <2277@holbertonschool.com>
2020-11-04 18:14:00 +0000

    Singly linked list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -2,5 +2,15 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
+	listint_t *aux, *next, *previous;
 
+	aux = *head;
+	while (aux != NULL)
+	{
+		next = aux->next;
+		aux->next = previous;
+		previous = aux;
+		aux = next;
+	}
+	return ((*head));
 }
