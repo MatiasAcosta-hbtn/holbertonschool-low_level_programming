e81da528c3f00942efbe6b04bee8ed44138dc0c1
Matias Acosta <2277@holbertonschool.com>
2020-11-04 12:46:39 +0000

    Fix ex 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -2,7 +2,7 @@
 /**
  * add_nodeint_end - add a node in the end
  * @head: a pointer to head
- * @n: the number 
+ * @n: the number
  * Return: Pointer to node with type listint_t
  */
 
@@ -10,15 +10,18 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *node, *aux;
 
-	if (*head == NULL)
-		return (NULL);
 	aux = *head;
 	node = malloc(sizeof(listint_t));
 	if (node == NULL)
 		return (NULL);
 	node->n = n;
 	node->next = NULL;
-	while(aux->next)
+	if (*head == NULL)
+	{
+		*head = node;
+		return (node);
+	}
+	while (aux->next)
 		aux = aux->next;
 	aux->next = node;
 	return (node);
