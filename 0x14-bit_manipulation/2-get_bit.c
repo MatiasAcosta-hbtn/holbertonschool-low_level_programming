58540daec1ef1aced1e4bcccfc98f498879faede
Matias Acosta <2277@holbertonschool.com>
2020-11-05 17:21:29 +0000

    Bit manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,9 +9,9 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int m;
-
-	if (n == 0 && index == 0)
-		return (n & 1);
+	
+	if (index > sizeof(n) * 8)
+		return (-1);
 	if (index == 0)
 		return (n & 1);
 	if (n == 0 && index != 0)
