6387a4f48312710160bd82138cc589996423b687
Matias Acosta <2277@holbertonschool.com>
2020-11-05 16:32:30 +0000

    Bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,13 +1,25 @@
 #include "holberton.h"
 #include <stdio.h>
+
+/**
+ * print_binary - print a number in binary
+ * @n: the number
+ * Return: Nothing
+ */
+
 void print_binary(unsigned long int n)
 {
-	unsigned long int last;
+	int flag = 0;
 
-	while(n > 1)
+	if (n == 0)
 	{
-		printf("%lu\n",n);
-		last = n;
-		n = n >> 1;
+		printf("0");
+	}
+	else if (n == 1)
+		printf("1");
+	else
+	{
+		print_binary(n >> 1);
+		printf("%lu", n & 1);
 	}
 }
