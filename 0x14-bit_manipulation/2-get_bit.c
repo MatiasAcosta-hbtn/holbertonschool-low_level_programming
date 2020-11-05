fbbb719afe5777047897bd1a0fc614add2b4bc16
Matias Acosta <2277@holbertonschool.com>
2020-11-05 17:05:34 +0000

    Bit manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * get_bit - print a number in binary
+ * @n: the number
+ * @index: the index
+ * Return: Num
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int m;
+
+	if (index == 0)
+		return (n & 1);
+	if (n == 0)
+		return (-1);
+	m = get_bit(n >> 1, index - 1);
+	return (m);
+}
