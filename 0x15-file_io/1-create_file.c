99e35cae5e2cd84ec9f4323d9b5fb18856ac97e8
Matias Acosta <2277@holbertonschool.com>
2020-11-09 14:51:25 -0300

    Files I/0 in C

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,14 +2,14 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file, letters, bytes;
+	int file, letters = 0, bytes = 0;
 
 	if (filename == NULL)
 		return (-1);
 	file = open(filename, O_CREAT | O_WRONLY, 0600);
 	while(*(text_content + letters))
 		letters++;
-	bytes = write(file, text_content, letters + 1)
+	bytes = write(file, text_content, letters + 1);
 	if(bytes == -1 || file == -1)
 	{
 		write(1, "fails", 5);
