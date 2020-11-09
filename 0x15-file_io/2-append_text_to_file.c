fed97b4e8f33eefa7036c7f98aa160f57ce0def5
Matias Acosta <2277@holbertonschool.com>
2020-11-09 17:03:47 -0300

    Files I/0 in C

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+ * append_text_to_file - append text to an existing file
+ * @filename: the name of the file
+ * @text_content: the context to append
+ * Return: 1 or -1 if fails
+ */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int file, bytes = 0, bytes_w;
@@ -7,11 +14,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	file = open(filename, O_RDWR | O_APPEND);
 	if (file == -1)
 		return (-1);
-	while(*(text_content + bytes))
-		bytes++;
-	bytes_w = write(file, text_content, bytes);
-	if (bytes_w == -1)
-		return (-1);
+	if (text_content != NULL)
+	{
+		while (*(text_content + bytes))
+			bytes++;
+		bytes_w = write(file, text_content, bytes);
+		if (bytes_w == -1)
+			return (-1);
+	}
 	close(file);
 	return (1);
 }
