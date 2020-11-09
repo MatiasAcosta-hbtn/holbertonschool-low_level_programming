7c6644972ddf0a4d0639d5e7e46fe4d92b7d1077
Matias Acosta <2277@holbertonschool.com>
2020-11-09 20:05:58 -0300

    Files I/0 in C

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include "holberton.h"
+#include <stdio.h>
+
+void close_file(int file)
+{
+	int c;
+
+	c = close(file);
+
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
+		exit(100);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int from, to, byte_r, byte_w, c_f, c_t;
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	from = open(argv[1], O_RDONLY);
+	byte_r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+	while (byte_r > 0)
+	{
+		if (from == -1 || byte_r == -1)
+		{
+			dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+		byte_w = write(to, buffer, byte_r);
+		if (to == -1 || byte_w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+		byte_r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+	}
+	free(buffer);
+	close_file(from);
+	close_file(to);
+	return (0);
+}
