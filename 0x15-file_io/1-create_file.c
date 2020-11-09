bf61b7ebe1df508ed7f07d18ed60c1d4b7704279
Matias Acosta <2277@holbertonschool.com>
2020-11-09 15:16:17 -0300

    Files I/0 in C

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+ * create_file - program that create a file
+ * @filename: the name of new file
+ * @text_content: the content
+ * Return: 1 or -1 if fail
+ */
+
 int create_file(const char *filename, char *text_content)
 {
 	int file, letters = 0, bytes = 0;
@@ -8,13 +15,16 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		while(*(text_content + letters))
+		while (*(text_content + letters))
 			letters++;
 	}
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	bytes = write(file, text_content, letters);
-	if(bytes == -1 || file == -1)
+	if (bytes == -1 || file == -1)
+	{
+		write(1, "fails", 5);
 		return (-1);
+	}
 	close(file);
 	return (1);
 }
