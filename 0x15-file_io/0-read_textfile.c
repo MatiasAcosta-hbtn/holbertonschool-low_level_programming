f534aa14640dd75e8b66f41526b7b0537ef7de1a
Matias Acosta <2277@holbertonschool.com>
2020-11-09 13:34:29 -0300

    Files I/0 in C

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * read_textfile - read a file
  * @filename: name of the file
@@ -11,25 +10,22 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
 	int file;
-	int bytes;
-	ssize_t byte;
+	ssize_t bytes_r, bytes_w;
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
-	if (!filename || letters < 0)
+	if (!filename)
 		return (0);
 	file = open(filename, O_RDONLY);
-	if (file == -1)
+	bytes_r = read(file, buffer, letters);
+	bytes_w = write(STDOUT_FILENO, buffer, bytes_r);
+	if (bytes_r == -1 || bytes_w == -1 || file == -1)
+	{
+		free(buffer);
 		return (0);
-	bytes = read(file, buffer, letters);
-	if (bytes == -1)
-		return (0);
-	bytes = write(1, buffer, bytes);
-	if (bytes == -1)
-		return (0);
+	}
 	close(file);
 	free(buffer);
-	byte = bytes;
-	return (byte);
+	return (bytes);
 }
