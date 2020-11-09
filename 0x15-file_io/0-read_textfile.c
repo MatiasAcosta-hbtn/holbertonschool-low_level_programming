e19286884ef7d86e5f74083ef22f6d883be69d0a
Matias Acosta <2277@holbertonschool.com>
2020-11-09 12:22:01 -0300

    First commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - read a file
+ * @filename: name of the file
+ * @letters: the amount letters to read
+ * Return: the amount of bytes readed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t lett;
+	char *buffer;
+	int file;
+	ssize_t bytes;
+
+	buffer = malloc(sizeof(char) * letters);
+	if (!filename)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
+	bytes = read(file, buffer, letters);
+	if (bytes == -1)
+		return (0);
+	write(1, buffer, bytes);
+	if (bytes == -1)
+		return (0);
+	close(file);
+	free(buffer);
+	return (bytes);
+}
