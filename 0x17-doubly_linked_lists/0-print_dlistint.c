cfd968015be23551427032a9c0bdfe143469446c
Matias Acosta <2277@holbertonschool.com>
2020-12-11 22:04:50 +0000

    Firs exercises

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,5 +8,17 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
+	size_t count = 0;
+	const dlistint_t *aux;
 
+	aux = h;
+	if (h == NULL)
+		return (0);
+	while (aux != NULL)
+	{
+		printf("%d\n", aux->n);
+		aux = aux->next;
+		count++;
+	}
+	return (count);
 }
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -5,6 +5,19 @@
  *
  *
  */
+
 size_t dlistint_len(const dlistint_t *h)
 {
+	size_t count = 0;
+	const dlistint_t *aux;
+
+	if (h == NULL)
+		return (0);
+	aux = h;
+	while(aux != NULL)
+	{
+		aux = aux->next;
+		count++;
+	}
+	return (count);
 }
