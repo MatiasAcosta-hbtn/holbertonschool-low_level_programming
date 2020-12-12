562b4d9d09e64ee95487904638bd172c1acc8502
Matias Acosta <2277@holbertonschool.com>
2020-12-12 19:22:08 -0300

    add ex 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,5 +1,18 @@
 #include "lists.h"
 
+dlistint_t *create_dnode(int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL)
+	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
+	return (new);
+}
+
 /**
  *
  *
@@ -8,5 +21,16 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *aux, *new;
 
+	aux = *head;
+	if (aux == NULL)
+		return (NULL);
+	new = create_node(n);
+	if (new == NULL)
+		return (NULL);
+	while (aux->prev != NULL)
+		aux = aux->prev;
+	new->next = *head;
+	*head = new;
 }
