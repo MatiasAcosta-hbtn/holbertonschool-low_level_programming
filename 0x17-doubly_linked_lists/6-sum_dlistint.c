152713329c4d250183181808f77e881327602535
Matias Acosta <2277@holbertonschool.com>
2020-12-13 01:06:44 -0300

    ex 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,12 +1,19 @@
 #include "lists.h"
 
 /** 
- *
- *
- *
+ * sum_dlistint - sum all nums in the list
+ * @head: the head of the list
+ * Return: the sum of the values in the list or 0 if is empty
  */
 
 int sum_dlistint(dlistint_t *head)
 {
+	int sum = 0;
 
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
