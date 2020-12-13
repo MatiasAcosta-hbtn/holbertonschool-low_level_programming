1ca994cbb9741ed1cd148de8a197b2817488f167
Matias Acosta <2277@holbertonschool.com>
2020-12-13 01:24:58 -0300

    Ex 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,12 +1,24 @@
 #include "lists.h"
 
-/** 
- *
- *
- *
+/**
+ * get_dnodeint_at_index - get a node with an index
+ * @head: the head of the list
+ * @index: the index
+ * Return: the address of the node in the index position
  */
- 
+
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	unsigned int n = 0;
+	dlistint_t *aux;
 
+	aux = head;
+	while (aux != NULL && index != 0)
+	{
+		aux = aux->next;
+		index--;
+	}
+	if (index != 0)
+		return (NULL);
+	return (aux);
 }
