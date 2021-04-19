79442038e9d7362ab53c82da45b60ebc7862cf02
Matias Acosta <2277@holbertonschool.com>
2021-04-19 17:02:19 -0300

    Initial commit

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - search an element using linear search algorithm
+ * @array: Pointer to the array
+ * @size: The size of the array
+ * @value: Value to find
+ */
+
+
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i = 0;
+    
+    if (array == NULL || size == 0)
+        return (-1);
+    
+    for (; i < size; i++)
+    {
+        printf("Value checked array[%li] = [%d]\n", i, array[i]);
+        if (array[i] == value)
+            return (i);
+    }
+
+    return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+#include <stdlib.h>
+#include <stdio.h>
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int binary_search_aux(int *array, int value, int start, int end, int size);
+#endif
