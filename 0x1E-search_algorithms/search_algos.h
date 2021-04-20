d93be842309eeaa80e58e8d48b189a3e65952e9d
Matias Acosta <2277@holbertonschool.com>
2021-04-20 18:49:01 -0300

    Task 104

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,4 +9,5 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int int_search_aux(int *array, int start, int end, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+int advanced_binary_aux(int *array, int value, int start, int end);
 #endif
