fe6b0a3f66cd317850efcb5981ee5e3ad4dd45db
Matias Acosta <2277@holbertonschool.com>
2021-04-21 12:51:20 -0300

    Fix 103

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -16,9 +16,15 @@ int exponential_search(int *array, size_t size, int value)
 		return (-1);
 
 	if (array[0] == value)
+	{
+		printf("Value checked array[0] = [%d]\n", array[0]);
 		return (0);
+	}
 	if (array[0] > value)
+	{	
+		printf("Value checked array[0] = [%d]\n", array[0]);
 		return (-1);
+	}
 	for (; i < size && array[i] <= value; i *= 2)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
