52c25aae3113d41ffe12c32331f051ab11a22a53
Matias Acosta <2277@holbertonschool.com>
2021-04-21 17:06:08 -0300

    New commit

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,13 +1,12 @@
 #include "search_algos.h"
 
 /**
- * linear_search - search an element using linear search algorithm
- * @array: Pointer to the array
- * @size: The size of the array
- * @value: Value to find
- * Return: index of the element or -1 if not founded
+ * linear_search - function to search for a given value in an array
+ * @array: pointer to the array to search
+ * @size: size of the array
+ * @value: value to search for
+ * Return: the index
  */
-
 
 int linear_search(int *array, size_t size, int value)
 {
