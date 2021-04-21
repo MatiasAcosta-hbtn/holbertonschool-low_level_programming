5865ef74a321cf3550fbee54732323343545f6a3
Matias Acosta <2277@holbertonschool.com>
2021-04-21 17:07:27 -0300

    Fix betty

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -25,10 +25,10 @@ int exponential_search(int *array, size_t size, int value);
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 skiplist_t *linear_skip(skiplist_t *list, int value);
 #endif
